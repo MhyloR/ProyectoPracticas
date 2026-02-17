6f29f5ff2af52253215c606719bf6ee1d24b5525
CamiloR <carodriguez@xm.com.co>
2026-02-17 16:02:35 -0500

    Modificacion de como se ingresan y extraen de Api

--- lectura_de_datos/Api/estraccion_de_datos_por_api.py
+++ lectura_de_datos/Api/estraccion_de_datos_por_api.py
@@ -2,16 +2,19 @@ import sys
 import os
 from pydataxm.pydatasimem import ReadSIMEM, CatalogSIMEM
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.dates as mpl_dates
 
-catalogo = CatalogSIMEM(catalog_type='Datasets')
-df_catalogo = catalogo.get_data()
 
-dataset_id = '95443E'
-fecha_inicio = '2024-09-28'
-fecha_fin = '2026-10-28'
-simem = ReadSIMEM(dataset_id, fecha_inicio, fecha_fin)
+def get_df(id, fecha_inicio, fecha_final):
+    catalogo = CatalogSIMEM(catalog_type='Datasets')
+    df_catalogo = catalogo.get_data()
 
-df_genreal = simem.main()
-df_genreal.to_csv("archivo.csv", index=False, sep=";")
+    dataset_id = id
+    fecha_inicio = fecha_inicio
+    fecha_fin = fecha_final
+    simem = ReadSIMEM(dataset_id, fecha_inicio, fecha_fin)
+
+    df_genreal = simem.main()
+    df_genreal.to_csv("archivo.csv")
+
+### EJM ###
+get_df('95443E','2024-09-28','2026-10-28')
