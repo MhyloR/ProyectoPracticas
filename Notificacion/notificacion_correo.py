e2dc13cf93174a85b7e45dcbd257bac019957320
CamiloR <carodriguez@xm.com.co>
2026-02-17 14:39:51 -0500

    Creacion de funcion para mandar Notificaciones

--- Notificacion/notificacion_correo.py
+++ Notificacion/notificacion_correo.py
@@ -2,26 +2,25 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-#Configuaracion de email
+def notificacion_de_mensaje(Sender, Reciver, password, data):
+    sender_email = Sender
+    reciver_email = Reciver
+    password = password
 
-sender_email = " "
-reciver_email = " "
-password = " "
+    # creamos el contenido del correo
 
-# creamos el contenido del correo
+    subject = "Reporte de Outlier"
+    body = data
 
-subject = "Reporte de Outlier"
-body = "Eventualmente en este lugar se encontrara algun reporte de outlier"
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = reciver_email
+    message['Subject'] = subject
 
-message = MIMEMultipart()
-message['From'] = sender_email
-message['To'] = reciver_email
-message['Subject'] = subject
+    message.attach(MIMEText(body, "plain"))
 
-message.attach(MIMEText(body, "plain"))
-
-with smtplib.SMTP("smtp.gmail.com",587) as server:
-    server.starttls()
-    server.login(sender_email, password)
-    server.send_message(message)
-print("Funcoino correctamente")
+    with smtplib.SMTP("smtp.gmail.com",587) as server:
+        server.starttls()
+        server.login(sender_email, password)
+        server.send_message(message)
+    print("Mensaje Enviado")
