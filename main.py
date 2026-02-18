da3a8c03da1aa592747914425c1a9a986edfdb9a
CamiloR <carodriguez@xm.com.co>
2026-02-18 15:45:04 -0500

    test main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from lectura_de_datos.Api.estraccion_de_datos_por_api import get_df
 from lectura_de_datos.Api.identificacion_de_columnas import obtener_namecolumns
 
-id = "055A4D"
+id = "75f675"
 inicio = "2025-01-01"
 fin = "2025-05-01"
 url = "https://www.simem.co/backend-files/api/detalle-datos-publicos?datasetId={dataset_id}"
@@ -10,3 +10,4 @@ print("Etapa1")
 get_df(id,inicio,fin)
 print("Etapa2")
 colums = obtener_namecolumns(id, url)
+print(colums)
